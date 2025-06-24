0b41598b73c41c6f3e43fd5a6c78b55ac9a45a9f
Vigneshkumar8610 <vigneshkumar22005@gmail.com>
2025-06-24 22:22:39 +0530

    added pagination

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -1,30 +1,26 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Movie from '#models/movie'
+import MovieStatus from '#models/movie_status'
+import MovieService from '#services/movie_service'
+import { movieFilterValidator } from '#validators/movie'
 
 export default class MoviesController {
-  async index({ view, auth }: HttpContext) {
-    await auth.check()
+  async index({ request, view }: HttpContext) {
+    const filters = await movieFilterValidator.validate(request.qs())
+    const movies = await MovieService.getFiltered(filters)
 
-    const comingSoon = await Movie.query()
-      .apply((scope) => scope.notReleased())
-      .preload('director')
-      .preload('writer')
-      .whereNotNull('releasedAt')
-      .orderBy('releasedAt')
-      .limit(3)
+    const movieStatuses = await MovieStatus.query().orderBy('name').select('id', 'name')
+    const movieSortOptions = MovieService.sortOptions
 
-    const recentlyReleased = await Movie.query()
-      .apply((scope) => scope.released())
-      .preload('director')
-      .preload('writer')
-      .orderBy('releasedAt', 'desc')
-      .limit(9)
-
-    return view.render('pages/home', { comingSoon, recentlyReleased })
+    return view.render('pages/movies/index', {
+      movies,
+      movieStatuses,
+      movieSortOptions,
+      filters,
+    })
   }
   async show({ view, params }: HttpContext) {
     const movie = await Movie.findByOrFail('slug', params.slug)
-
     const cast = await movie.related('castMembers').query().orderBy('pivot_sort_order')
     const crew = await movie
       .related('crewMembers')
@@ -34,6 +30,7 @@ export default class MoviesController {
 
     await movie.load('director')
     await movie.load('writer')
+
     return view.render('pages/movies/show', { movie, cast, crew })
   }
 }
